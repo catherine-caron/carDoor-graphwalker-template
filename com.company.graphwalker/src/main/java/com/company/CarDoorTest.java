f07ec0a0a6dfffd7130dc716c2d4e22327ab572b
Aren Babikian <aren.babikian@mail.mcgill.ca>
2021-11-10 01:41:18 -0500

    Add sample solution

--- com.company.graphwalker/src/main/java/com/company/CarDoorTest.java
+++ com.company.graphwalker/src/main/java/com/company/CarDoorTest.java
@@ -2,18 +2,52 @@ package com.company;
 
 import org.graphwalker.core.machine.ExecutionContext;
 import org.graphwalker.java.annotation.GraphWalker;
+import org.junit.Assert;
 
 @GraphWalker(value = "random(edge_coverage(100))", start = "v_Closed" )
 public class CarDoorTest extends ExecutionContext implements CarDoorModel {
 
     private final CarDoor door = new CarDoor();
     
-    // This is just an example
     @Override
     public void e_unlock(){
         System.out.println("e_unlock");
         door.unlockDoor();
     }
 
-    // TODO: complete this file
+    @Override
+    public void e_lock(){
+        System.out.println("e_unlock");
+        door.lockDoor();
+    }
+
+    @Override
+    public void e_close(){
+        System.out.println("e_unlock");
+        door.closeDoor();
+    }
+
+    @Override
+    public void e_open(){
+        System.out.println("e_unlock");
+        door.openDoor();
+    }
+
+    @Override
+    public void v_Closed(){
+        Assert.assertTrue(door.getStatus() == CarDoorStatus.CLOSED);
+        System.out.println("v_Closed");
+    }
+
+    @Override
+    public void v_Opened(){
+        Assert.assertTrue(door.getStatus() == CarDoorStatus.OPENED);
+        System.out.println("v_Opened");
+    }
+
+    @Override
+    public void v_Locked(){
+        Assert.assertTrue(door.getStatus() == CarDoorStatus.LOCKED);
+        System.out.println("v_Locked");
+    }
 }
